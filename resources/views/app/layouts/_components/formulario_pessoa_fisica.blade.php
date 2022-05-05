8c43ff9360b24df6d1b005004b3d68b763f5586b
David Ferreira <ferreiradavid361@gmail.com>
2022-05-05 11:51:29 -0300

    Finalizando formulários de cadastro de clientes PF e PJ

--- resources/views/app/layouts/_components/formulario_pessoa_fisica.blade.php
+++ resources/views/app/layouts/_components/formulario_pessoa_fisica.blade.php
@@ -3,40 +3,44 @@
 
     <form action="{{ route('app.clientes-cadastro') }}" class="row g-1 justify-content-center" method="POST">
         @csrf
+
+        {{-- Informa o tipo de pessoa (PF=1 ou PJ=2) para o banco de dados --}}
+        <input type="hidden" name="tipo_pessoa" value="1">
+
         <div class="form-floating mb-3 col-md-5 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Nome" required>
+            <input type="text" name="nome" class="form-control" id="floatingInput" placeholder="Nome" required>
             <label for="floatingInput">Nome</label>
         </div>
         <div class="form-floating mb-3 col-md-3 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="CPF" required>
+            <input type="text" name="cpf" class="form-control" id="floatingInput" placeholder="CPF" required>
             <label for="floatingInput">CPF</label>
         </div>
         <div class="form-floating mb-3 col-md-5 ms-1">
-            <input type="email" class="form-control" id="floatingInput" placeholder="Email" required>
+            <input type="email" name="email" class="form-control" id="floatingInput" placeholder="Email" required>
             <label for="floatingInput">Email</label>
         </div>
         <div class="form-floating mb-3 col-md-3 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Telefone" required>
+            <input type="text" name="telefone" class="form-control" id="floatingInput" placeholder="Telefone" required>
             <label for="floatingInput">Telefone</label>
         </div>
         <div class="form-floating mb-3 col-md-4 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Logradouro" required>
+            <input type="text" name="logradouro" class="form-control" id="floatingInput" placeholder="Logradouro" required>
             <label for="floatingInput">Logradouro</label>
         </div>
         <div class="form-floating mb-3 col-md-4 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Bairro" required>
+            <input type="text" name="bairro" class="form-control" id="floatingInput" placeholder="Bairro" required>
             <label for="floatingInput">Bairro</label>
         </div>
         <div class="form-floating mb-3 col-md-5 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Cidade" required>
+            <input type="text" name="cidade" class="form-control" id="floatingInput" placeholder="Cidade" required>
             <label for="floatingInput">Cidade</label>
         </div>
         <div class="form-floating mb-3 col-md-3 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="CEP" required>
+            <input type="text" name="cep" class="form-control" id="floatingInput" placeholder="CEP" required>
             <label for="floatingInput">CEP</label>
         </div>
         <div class="form-floating mb-3 col-md-8 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Estado" required>
+            <input type="text" name="estado" class="form-control" id="floatingInput" placeholder="Estado" required>
             <label for="floatingInput">Estado</label>
         </div>
 
--- resources/views/app/layouts/_components/formulario_pessoa_juridica.blade.php
+++ resources/views/app/layouts/_components/formulario_pessoa_juridica.blade.php
@@ -3,48 +3,52 @@
 
     <form action="{{ route('app.clientes-cadastro') }}" class="row g-1 justify-content-center" method="POST">
         @csrf
+
+        {{-- Informa o tipo de pessoa (PF=1 ou PJ=2) para o banco de dados --}}
+        <input type="hidden" name="tipo_pessoa" value="2">
+
         <div class="form-floating mb-3 col-md-5 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Razão social" required>
+            <input type="text" name="razaoSocial" class="form-control" id="floatingInput" placeholder="Razão social" required>
             <label for="floatingInput">Razão social</label>
         </div>
         <div class="form-floating mb-3 col-md-2 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="CNPJ" required>
+            <input type="text" name="cnpj" class="form-control" id="floatingInput" placeholder="CNPJ" required>
             <label for="floatingInput">CNPJ</label>
         </div>
         <div class="form-floating mb-3 col-md-5 ms-1">
-            <input type="email" class="form-control" id="floatingInput" placeholder="Email" required>
+            <input type="email" name="email" class="form-control" id="floatingInput" placeholder="Email" required>
             <label for="floatingInput">Email</label>
         </div>
         <div class="form-floating mb-3 col-md-2 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Telefone" required>
+            <input type="text" name="telefone" class="form-control" id="floatingInput" placeholder="Telefone" required>
             <label for="floatingInput">Telefone</label>
         </div>
         <div class="form-floating mb-3 col-md-7 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Logradouro" required>
+            <input type="text" name="logradouro" class="form-control" id="floatingInput" placeholder="Logradouro" required>
             <label for="floatingInput">Logradouro</label>
         </div>
         <div class="form-floating mb-3 col-md-5 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Bairro" required>
+            <input type="text" name="bairro" class="form-control" id="floatingInput" placeholder="Bairro" required>
             <label for="floatingInput">Bairro</label>
         </div>
         <div class="form-floating mb-3 col-md-2 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="CEP" required>
+            <input type="text" name="cep" class="form-control" id="floatingInput" placeholder="CEP" required>
             <label for="floatingInput">CEP</label>
         </div>
         <div class="form-floating mb-3 col-md-5 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Cidade" required>
+            <input type="text" name="cidade" class="form-control" id="floatingInput" placeholder="Cidade" required>
             <label for="floatingInput">Cidade</label>
         </div>
         <div class="form-floating mb-3 col-md-2 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Estado" required>
+            <input type="text" name="estado" class="form-control" id="floatingInput" placeholder="Estado" required>
             <label for="floatingInput">Estado</label>
         </div>
         <div class="form-floating mb-3 col-md-5 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Nome responsável" required>
+            <input type="text" name="nomeResponsavel" class="form-control" id="floatingInput" placeholder="Nome responsável" required>
             <label for="floatingInput">Nome responsável</label>
         </div>
         <div class="form-floating mb-3 col-md-2 ms-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Telefone responsável" required>
+            <input type="text" name="telefoneResponsavel" class="form-control" id="floatingInput" placeholder="Telefone responsável" required>
             <label for="floatingInput">Telefone responsável</label>
         </div>
 
