688d7a336578299c34dc04d3d6c94446b8bb9fc9
David Ferreira <ferreiradavid361@gmail.com>
2022-05-12 11:28:39 -0300

    Arrumando cabeçalho e rodapé

--- resources/views/app/error404.blade.php
+++ resources/views/app/error404.blade.php
@@ -1,12 +1,14 @@
+
 {{-- Permitindo que app/_componets/cabecalho_e_rodape.blade.php possa usar meu template --}}
 @extends('app.layouts._components.cabecalho_e_rodape')
 @extends('app.layouts._components.head_login')
 
 
-
 @section('titulo', 'Página não encontrada')
 
 @section('counteudo')
+
+@include('app.layouts._partials.barra_de_navegacao')  {{-- Inlcuindo barra de navegação --}}
 
 <div class="container regua">
     <div class="row">
@@ -14,12 +16,12 @@
             <h1>Oops!</h1>
             <h2>Erro 404</h2>
             <p>Desculpe, endereço inválido, Página não encontrada!</p>
-            <a href="{{ url('/') }}" class="btn btn-primary btn-sm">
+            <a href="{{ url('/app/home') }}" class="btn btn-primary btn-sm">
                 Início</a>
         </div>
     </div>
 </div>
 
+@include('app.layouts._partials.rodape')  {{-- Inlcuindo rodapé --}}
+
 @endsection
-
-
